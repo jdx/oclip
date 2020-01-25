e8c786ecba0a74553b03a0598435eedb1c8d9bf7
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2020-01-25 05:12:40 -0800

    lots of changes

--- src/command.test.ts
+++ src/command.test.ts
@@ -7,25 +7,27 @@ describe('run', () => {
 
   test('runs with no arguments', () => {
     process.argv = ['node', './script']
-    return oclip().run()
+    return oclip().parse()
   })
 
   test('runs with just a run function', async () => {
     process.argv = ['node', './script']
     const fn = jest.fn()
-    await oclip().run(() => fn('abc'))
+    await oclip({
+      run: () => fn('abc')
+    }).parse()
     expect(fn).toBeCalledWith('abc')
   })
 
   test('passes through return value', () => {
-    return expect(oclip().run([], () => 123))
-      .resolves.toEqual(123)
+    return expect(oclip({
+      run: () => 123
+    }).parse([])).resolves.toEqual(123)
   })
 
   test('gets homedir', () => {
-    return oclip()
-      .run([], ({ctx}) => {
-        expect(ctx.dirs.home).toMatch(/^\//)
-      })
+    return oclip({
+      run: ({ctx}) => expect(ctx.dirs.home).toMatch(/^\//)
+    }).parse([])
   })
 })
--- src/command.ts
+++ src/command.ts
@@ -1,19 +1,17 @@
-import { Arg, RestArg } from './args'
+import { Arg, RestArg, arg } from './args'
 import { Flags } from './flags'
 import { FullOptions, Options } from './options'
 import parse from './parse'
 import * as os from 'os'
 
-export class Command<TArgs extends any[], F extends Flags> {
-  constructor(readonly options: FullOptions<{[N in keyof TArgs]: Arg<TArgs[N]>}, F>) {
+export class Command<F extends Flags, R> {
+  constructor(readonly options: FullOptions<any, F, R, []>) {
     this.options = this.options || {}
     this.options.args = this.options.args || [] as any
+    this.options.run = this.options.run || (() => {})
   }
 
-  async run<R>(argv: string[] | CommandRun<TArgs, any, F, R> = process.argv, run?: CommandRun<TArgs, any, F, R>) {
-    if (typeof argv === 'function') run = argv
-    if (!argv || typeof argv === 'function') argv = process.argv
-    if (argv === process.argv) argv = argv.slice(2)
+  async parse(argv = process.argv.slice(2)) {
     const {args, flags} = await parse(this.options, argv) as any
     const ctx = {
       dirs: {
@@ -21,44 +19,26 @@ export class Command<TArgs extends any[], F extends Flags> {
       },
       command: this,
     }
-    if (!run) return undefined as any
-    const result: any = await run({args, flags, ctx})
+    const result: any = await this.options.run({args, flags, ctx})
     return result
   }
 }
 
-export const oclip: Oclip = <O extends Options<any, any>>(options: O) => new Command(options) as any
+export const oclip: Oclip = <O extends Options<any, any, any, any>>(options: O) => new Command(options as any) as any
 
 export type ArgVal<A extends Arg<any>> = A extends {required: false} ? ReturnType<A['parse']> | undefined : ReturnType<A['parse']>
 
 export interface Oclip {
-  <TFlags extends Flags, A1 extends Arg<any>, A2 extends Arg<any>, A3 extends Arg<any>, A4 extends Arg<any>, A5 extends RestArg<any>>(options: Options<[A1, A2, A3, A4, A5], TFlags>): Command<[ArgVal<A1>, ArgVal<A2>, ArgVal<A3>, ArgVal<A4>, ...ArgVal<A5>[]], TFlags>
-  <TFlags extends Flags, A1 extends Arg<any>, A2 extends Arg<any>, A3 extends Arg<any>, A4 extends Arg<any>, A5 extends Arg<any>>(options: Options<[A1, A2, A3, A4, A5], TFlags>): Command<[ArgVal<A1>, ArgVal<A2>, ArgVal<A3>, ArgVal<A4>, ArgVal<A5>], TFlags>
-  <TFlags extends Flags, A1 extends Arg<any>, A2 extends Arg<any>, A3 extends Arg<any>, A4 extends RestArg<any>>(options: Options<[A1, A2, A3, A4], TFlags>): Command<[ArgVal<A1>, ArgVal<A2>, ArgVal<A3>, ...ArgVal<A4>[]], TFlags>
-  <TFlags extends Flags, A1 extends Arg<any>, A2 extends Arg<any>, A3 extends Arg<any>, A4 extends Arg<any>>(options: Options<[A1, A2, A3, A4], TFlags>): Command<[ArgVal<A1>, ArgVal<A2>, ArgVal<A3>, ArgVal<A4>], TFlags>
-  <TFlags extends Flags, A1 extends Arg<any>, A2 extends Arg<any>, A3 extends RestArg<any>>(options: Options<[A1, A2, A3], TFlags>): Command<[ArgVal<A1>, ArgVal<A2>, ...ArgVal<A3>[]], TFlags>
-  <TFlags extends Flags, A1 extends Arg<any>, A2 extends Arg<any>, A3 extends Arg<any>>(options: Options<[A1, A2, A3], TFlags>): Command<[ArgVal<A1>, ArgVal<A2>, ArgVal<A3>], TFlags>
-  <TFlags extends Flags, A1 extends Arg<any>, A2 extends RestArg<any>>(options: Options<[A1, A2], TFlags>): Command<[ArgVal<A1>, ...ArgVal<A2>[]], TFlags>
-  <TFlags extends Flags, A1 extends Arg<any>, A2 extends Arg<any>>(options: Options<[A1, A2], TFlags>): Command<[ArgVal<A1>, ArgVal<A2>], TFlags>
-  <TFlags extends Flags, A1 extends RestArg<any>>(options: Options<[A1], TFlags>): Command<ArgVal<A1>[], TFlags>
-  <TFlags extends Flags, A1 extends Arg<any>>(options: Options<[A1], TFlags>): Command<[ArgVal<A1>], TFlags>
-  <TFlags extends Flags>(options?: Options<[], TFlags>): Command<[], Flags>
-  <TFlags extends Flags, A extends Arg<any>>(options?: Options<A[], TFlags>): Command<ArgVal<A>[], TFlags>
-}
-
-export interface CommandRun<TArgs extends any[], A extends Arg<any>[], F extends Flags, R> {
-  (params: RunParams<TArgs, A, F>): Promise<R> | R
-}
-
-export interface RunParams<TArgs extends any[], A extends Arg<any>[], F extends Flags> {
-  args: TArgs
-  flags: {}
-  ctx: RunContext<A, F>
-}
-
-export interface RunContext<A extends Arg<any>[], F extends Flags> {
-  dirs: {
-    home: string
-  }
-  command: Command<A, F>
+  <F extends Flags, A1 extends Arg<any>, A2 extends Arg<any>, A3 extends Arg<any>, A4 extends Arg<any>, A5 extends RestArg<any>, R>(options: Options<[A1, A2, A3, A4, A5], F, R, [ArgVal<A1>, ArgVal<A2>, ArgVal<A3>, ArgVal<A4>, ...ArgVal<A5>[]]>): Command<F, R>
+  <F extends Flags, A1 extends Arg<any>, A2 extends Arg<any>, A3 extends Arg<any>, A4 extends Arg<any>, A5 extends Arg<any>, R>(options: Options<[A1, A2, A3, A4, A5], F, R, [ArgVal<A1>, ArgVal<A2>, ArgVal<A3>, ArgVal<A4>, ArgVal<A5>]>): Command<F, R>
+  <F extends Flags, A1 extends Arg<any>, A2 extends Arg<any>, A3 extends Arg<any>, A4 extends RestArg<any>, R>(options: Options<[A1, A2, A3, A4], F, R, [ArgVal<A1>, ArgVal<A2>, ArgVal<A3>, ...ArgVal<A4>[]]>): Command<F, R>
+  <F extends Flags, A1 extends Arg<any>, A2 extends Arg<any>, A3 extends Arg<any>, A4 extends Arg<any>, R>(options: Options<[A1, A2, A3, A4], F, R, [ArgVal<A1>, ArgVal<A2>, ArgVal<A3>, ArgVal<A4>]>): Command<F, R>
+  <F extends Flags, A1 extends Arg<any>, A2 extends Arg<any>, A3 extends RestArg<any>, R>(options: Options<[A1, A2, A3], F, R, [ArgVal<A1>, ArgVal<A2>, ...ArgVal<A3>[]]>): Command<F, R>
+  <F extends Flags, A1 extends Arg<any>, A2 extends Arg<any>, A3 extends Arg<any>, R>(options: Options<[A1, A2, A3], F, R, [ArgVal<A1>, ArgVal<A2>, ArgVal<A3>]>): Command<F, R>
+  <F extends Flags, A1 extends Arg<any>, A2 extends RestArg<any>, R>(options: Options<[A1, A2], F, R, [ArgVal<A1>, ...ArgVal<A2>[]]>): Command<F, R>
+  <F extends Flags, A1 extends Arg<any>, A2 extends Arg<any>, R>(options: Options<[A1, A2], F, R, [ArgVal<A1>, ArgVal<A2>]>): Command<F, R>
+  <F extends Flags, A1 extends RestArg<any>, R>(options: Options<[A1], F, R, ArgVal<A1>[]>): Command<F, R>
+  <F extends Flags, A1 extends Arg<any>, R>(options: Options<[A1], F, R, [ArgVal<A1>]>): Command<F, R>
+  <F extends Flags, R>(options?: Options<[], F, R, []>): Command<F, R>
+  <F extends Flags, A extends Arg<any>, R>(options?: Options<A[], F, R, ArgVal<A>[]>): Command<F, R>
 }
