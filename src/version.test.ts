cb4769eb5c98fdcc9872087fd42cbc7eaba71749
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2020-01-25 06:40:57 -0800

    added basic version command

--- src/version.test.ts
+++ src/version.test.ts
@@ -1,7 +1,7 @@
 import {oclip} from '.'
 
 test('version', async () => {
-  const spy = jest.spyOn(console, 'log')
+  const spy = jest.spyOn(console, 'log').mockImplementationOnce(() => {})
   await oclip({run: () => {}}).parse(['--version'])
   expect(spy).toHaveBeenCalledWith('oclip version: 0.0.0')
 })
