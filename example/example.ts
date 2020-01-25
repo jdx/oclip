745ef4680ec790489f0aadea58e6b7a66f81bf49
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2020-01-25 08:36:01 -0800

    test example

--- example/example.ts
+++ example/example.ts
@@ -1,8 +1,8 @@
 import {oclip, arg} from 'oclip'
 
 oclip({
-  args: [arg('arg1'), arg.optional('arg2')],
+  args: [arg('ARG_1'), arg.optional('ARG_2')],
   run({args}) {
     console.dir({args})
   }
-}).parse()
+}).exec()
