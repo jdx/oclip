409a9513ec103f3c13b00b6858558b59adee313c
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2020-11-12 07:17:50 -0800

    wip

--- scripts/gen_readme.ts
+++ scripts/gen_readme.ts
@@ -1,22 +1,28 @@
+import { command } from "../mod.ts";
 import { stdout } from "./helpers/exec.ts";
 
-async function apiDocumentation(): Promise<string> {
-  const doc = await stdout(["deno", "doc", "mod.ts"]);
-  return [
-    "```",
-    doc,
-    "```",
-  ].join("\n");
-}
+const cmd = command({
+  async run() {
+    async function apiDocumentation(): Promise<string> {
+      const doc = await stdout(["deno", "doc", "mod.ts"]);
+      return [
+        "```",
+        doc,
+        "```",
+      ].join("\n");
+    }
 
-const contents = [
-  "# oclip",
-  "",
-  "## API Documentation",
-  "",
-  await apiDocumentation(),
-  "Made by Jeff Dickey",
-  "",
-];
+    const contents = [
+      "# oclip",
+      "",
+      "## API Documentation",
+      "",
+      await apiDocumentation(),
+      "Made by Jeff Dickey",
+      "",
+    ];
 
-await Deno.writeTextFile("README.md", contents.join("\n"));
+    await Deno.writeTextFile("README.md", contents.join("\n"));
+  },
+  main: import.meta.main,
+});
--- src/command.ts
+++ src/command.ts
@@ -3,6 +3,14 @@
 import * as arg from "./arg.ts";
 import { Flags } from "./flag.ts";
 
+/**
+ * @callback CommandRunFn
+ * @param {any}   - the parsed positional arguments. Usually these will be strings but can
+ * be parsed into other types.
+ * @param {Flags} - a key-value object for flag values
+ * @returns {Promise<any>} - the command can return anything and it will propagate down to the caller. It
+ * will always run async.
+ */
 export type CommandRunFn<A extends arg.List, F extends Flags, R> = (
   args: arg.ListToResults<A>,
 ) => Promise<R> | R;
@@ -11,6 +19,7 @@ export interface CommandOptions<A extends arg.List, F extends Flags, R> {
   flags?: F;
   description?: string;
   hidden?: boolean;
+  main?: boolean;
   run: CommandRunFn<A, F, R>;
 }
 
@@ -40,6 +49,12 @@ export class Command<A extends arg.List, F extends Flags, R> {
 
 /**
  * Defines a CLI command
+ *
+ * @param {Object} options
+ * @param {Boolean} options.main - pass true to start the command immediately.
+ * You can pass import.meta.main in which is true when the script is being run directly.
+ * This allows us to have a command be imported for tests but also run directly without a wrapper.
+ * @param {CommandRunFn} options.run - Called when the command is run with parsed args + flags.
  */
 export function command<A extends arg.List, F extends Flags, R>(
   options: CommandOptions<A, F, R>,
