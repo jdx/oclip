852e1bb7770c83fec91fe02f7ace2518af2eb2f0
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2020-11-12 21:45:36 -0800

    wip

--- scripts/gen_readme.ts
+++ scripts/gen_readme.ts
@@ -1,30 +1,32 @@
-import { command } from "../mod.ts";
-import { stdout } from "./helpers/exec.ts";
+// import { command } from "../mod.ts";
+// import { stdout } from "./helpers/exec.ts";
 
-export default command({
-  async run() {
-    async function apiDocumentation(): Promise<string> {
-      const doc = await stdout(["deno", "doc", "mod.ts"]);
-      return [
-        "```",
-        doc,
-        "```",
-      ].join("\n");
-    }
+// export default command({
+//   async run() {
+//     async function apiDocumentation(): Promise<string> {
+//       const doc = await stdout(["deno", "doc", "mod.ts"]);
+//       return [
+//         "```",
+//         doc,
+//         "```",
+//       ].join("\n");
+//     }
 
-    const contents = [
-      "# oclip",
-      "",
-      "## API Documentation",
-      "",
-      await apiDocumentation(),
-      "Made by Jeff Dickey",
-      "",
-    ];
+//     const contents = [
+//       "# oclip",
+//       "",
+//       "## API Documentation",
+//       "",
+//       await apiDocumentation(),
+//       "Made by Jeff Dickey",
+//       "",
+//     ];
 
-    await Deno.writeTextFile("README.md", contents.join("\n"));
+//     await Deno.writeTextFile("README.md", contents.join("\n"));
 
-    console.log(`Updated README.md`);
-  },
-  main: import.meta.main,
-});
+//     console.log(`Updated README.md`);
+//   },
+//   main: import.meta.main,
+// });
+
+console.log('yay')
